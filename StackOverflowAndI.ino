8354b914ce85fc36fc933f8d266535875ec057b9
Samantha Norrie <49314677+Samantha-norrie@users.noreply.github.com>
2024-10-19 10:59:37 -0700

    all components connected, debugging workflow

--- StackOverflowAndI.ino
+++ StackOverflowAndI.ino
@@ -1,6 +1,11 @@
 #include "SevSeg.h"
 SevSeg sevseg;
 
+// Joystick Pins
+const int SW_pin = 2;
+const int X_pin = A0;
+const int Y_pin = A1;
+
 // LED Pins
 const int Q0_PIN = 13;
 const int Q1_PIN = 12;
@@ -22,7 +27,17 @@ int gameState = 0;
 
 int currentQuestion = 0;
 
+int q0Code = -1;
+int q0Index = 0;
+int *q0Values;
 
+int q1Code = -1;
+int q1Index = 0;
+int *q1Values;
+
+int q2Code = -1;
+int q2Index = 0;
+int *q2Values;
 
 // Question Codes
 const int EDUCATION = 1100;
@@ -39,10 +54,18 @@ const int AI_BENEFIT_VALUES[] = {81, 62, 59, 30, 25, 8};
 const int AI_IN_DEV_VALUES[] = {82, 68, 57, 40, 13, 27};
 const int AI_CHALLENGES_VALUES[] = {66, 26, 32, 31, 63, 13};
 
+const int QUESTION_VALUES_LIST[][7] = {EDUCATION_VALUES, LANGUAGES_VALUES, OP_SYSTEM_VALUES,
+AI_BENEFIT_VALUES, AI_IN_DEV_VALUES, AI_CHALLENGES_VALUES};
+
 const int QUESTION_CODE_LIST[] = {EDUCATION, LANGUAGES, OP_SYSTEM, 
   AI_BENEFIT, AI_IN_DEV, AI_CHALLENGES};
+
+int questionCodeIndex = 0;
+
 void setup() {
-  // put your setup code here, to run once:
+  pinMode(SW_pin, INPUT);
+  digitalWrite(SW_pin, HIGH);
+
   pinMode(Q0_PIN, OUTPUT);
   pinMode(Q1_PIN, OUTPUT);
   pinMode(Q2_PIN, OUTPUT);
@@ -52,8 +75,68 @@ void setup() {
   sevseg.setBrightness(90);
 }
 
-// Handle joystick
+void reset() {
+  gameState = 0;
+  currentQuestion = 0;
 
+  q0Code = -1;
+  q0Index = 0;
+
+  q1Code = -1;
+  q1Index = 0;
+
+  q2Code = -1;
+  q2Index = 0;
+}
+
+// Handle joystick
+void handleJoystick() {
+  int press = digitalRead(SW_pin);
+  int xCoord = analogRead(X_pin);
+  int yCoord = analogRead(Y_pin);
+
+  if (xCoord <= 400) {
+    currentQuestion > 0? currentQuestion = currentQuestion-1: currentQuestion = 2;
+  } else if (xCoord >= 700) {
+    currentQuestion < 2? currentQuestion = currentQuestion+1: currentQuestion = 0;
+  }
+
+  if (yCoord >= 700) {
+    if (gameState == 0) {
+      questionCodeIndex < 0? questionCodeIndex = questionCodeIndex-1: questionCodeIndex;
+    } else {
+      if (currentQuestion == 0) {
+        q0Index > 0? q0Index = q0Index - 1: q0Index = sizeof(q0Values) / sizeof(q0Values[0])-1;
+      } else if (currentQuestion == 1) {
+        q1Index > 0? q1Index = q1Index - 1: q1Index = sizeof(q1Values) / sizeof(q1Values[0])-1;
+      } else {
+        q2Index > 0? q2Index = q2Index - 1: q2Index = sizeof(q2Values) / sizeof(q2Values[0])-1;
+      }
+    }
+  }
+
+  if (press == 0) {
+    if (gameState == 0) {
+      if (q0Code != -1 && q1Code != -1 && q2Code != -1) {
+        gameState = 1;
+      } else if (currentQuestion == 0) {
+        q0Code = QUESTION_CODE_LIST[questionCodeIndex];
+        q0Values = QUESTION_VALUES_LIST[questionCodeIndex];
+        currentQuestion = currentQuestion+1;
+      } else if (currentQuestion == 1) {
+        q1Code = QUESTION_CODE_LIST[questionCodeIndex];
+        q1Values = QUESTION_VALUES_LIST[questionCodeIndex];
+        currentQuestion = currentQuestion+1;
+      } else {
+        q2Code = QUESTION_CODE_LIST[questionCodeIndex];
+        q2Values = QUESTION_VALUES_LIST[questionCodeIndex];
+      }
+    } else {
+      reset();
+    } 
+  }
+
+}
 // Control LEDs
 void updateLEDs() {
   digitalWrite(Q0_PIN, currentQuestion == 0? HIGH: LOW);
@@ -61,10 +144,24 @@ void updateLEDs() {
   digitalWrite(Q2_PIN, currentQuestion == 2? HIGH: LOW);
 }
 
-
+void updateScreen() {
+  if (gameState == 0) {
+      sevseg.setNumber(QUESTION_CODE_LIST[questionCodeIndex]);
+  } else {
+    if (currentQuestion == 0) {
+      sevseg.setNumber(q0Values[q0Index]);
+    } else if (currentQuestion == 1) {
+      sevseg.setNumber(q1Values[q1Index]);
+    } else {
+      sevseg.setNumber(q2Values[q2Index]);
+    }
+  }
+  sevseg.refreshDisplay();
+}
 
 void loop() {
-  sevseg.setNumber(1100);
-  sevseg.refreshDisplay();
 
+  handleJoystick();
+  updateScreen();
+  updateLEDs();
 }
