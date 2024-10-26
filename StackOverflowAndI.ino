6800535edc4645e8ca3232ab286787294781c930
Samantha Norrie <49314677+Samantha-norrie@users.noreply.github.com>
2024-10-26 11:08:29 -0700

    Add files via upload

--- StackOverflowAndI.ino
+++ StackOverflowAndI.ino
@@ -12,8 +12,8 @@ const int Q1_PIN = A3;
 const int Q2_PIN = A4;
 
 // 4-Digit 7 Segment Pins
-  byte segmentPins[] = {2, 3, 4, 5, 6, 7, 8, 9}; // Pins for a, b, c, d, e, f, g, DP
-  byte digitPins[] = {10, 11, 12, 13}; 
+byte segmentPins[] = {2, 3, 4, 5, 6, 7, 8, 9}; // Pins for a, b, c, d, e, f, g, DP
+byte digitPins[] = {10, 11, 12, 13}; 
 
 const byte numDigits = 4;
 
@@ -23,24 +23,26 @@ Question select -> 0
 Question Viewing -> 1
 */
 int gameState = 0;
-int bigNumber = 9999;
 
 int currentQuestion = 0;
 
 int q0Code = -1;
 int q0Index = 0;
+int q0Size = -1;
 int *q0Values;
 
 int q1Code = -1;
 int q1Index = 0;
+int q1Size = -1;
 int *q1Values;
 
 int q2Code = -1;
 int q2Index = 0;
+int q2Size = -1;
 int *q2Values;
 
 // Question Codes
-const int EDUCATION = 1900;
+const int EDUCATION = 1100;
 const int LANGUAGES = 2101;
 const int OP_SYSTEM = 2111;
 const int AI_BENEFIT = 3201;
@@ -54,13 +56,16 @@ const int AI_BENEFIT_VALUES[] = {81, 62, 59, 30, 25, 8};
 const int AI_IN_DEV_VALUES[] = {82, 68, 57, 40, 13, 27};
 const int AI_CHALLENGES_VALUES[] = {66, 26, 32, 31, 63, 13};
 
-const int QUESTION_VALUES_LIST[][7] = {EDUCATION_VALUES, LANGUAGES_VALUES, OP_SYSTEM_VALUES,
+const int CODE_LIST_SIZE = 6;
+
+const int QUESTION_VALUES_LIST[][CODE_LIST_SIZE] = {EDUCATION_VALUES, LANGUAGES_VALUES, OP_SYSTEM_VALUES,
 AI_BENEFIT_VALUES, AI_IN_DEV_VALUES, AI_CHALLENGES_VALUES};
 
 const int QUESTION_CODE_LIST[] = {EDUCATION, LANGUAGES, OP_SYSTEM, 
   AI_BENEFIT, AI_IN_DEV, AI_CHALLENGES};
 
-const int CODE_LIST_SIZE = 6;
+// temp workaround
+const int QUESTION_VALUES_LIST_SIZES[CODE_LIST_SIZE] = {5, 5, 5, 6, 6, 6};
 
 int questionCodeIndex = 0;
 
@@ -83,12 +88,15 @@ void reset() {
 
   q0Code = -1;
   q0Index = 0;
+  q0Size = -1;
 
   q1Code = -1;
   q1Index = 0;
+  q1Size = -1;
 
   q2Code = -1;
   q2Index = 0;
+  q2Size = -1;
 }
 
 // Handle joystick
@@ -128,8 +136,6 @@ void handleVertical(bool up) {
   }
 }
 
-
-
 void handlePress() {
   if (gameState == 0) {
     if (q0Code != -1 && q1Code != -1 && q2Code != -1) {
@@ -137,14 +143,17 @@ void handlePress() {
     } else if (currentQuestion == 0) {
       q0Code = QUESTION_CODE_LIST[questionCodeIndex];
       q0Values = QUESTION_VALUES_LIST[questionCodeIndex];
+      q0Size = QUESTION_VALUES_LIST_SIZES[questionCodeIndex];
       currentQuestion = currentQuestion+1;
     } else if (currentQuestion == 1) {
       q1Code = QUESTION_CODE_LIST[questionCodeIndex];
       q1Values = QUESTION_VALUES_LIST[questionCodeIndex];
+      q1Size = QUESTION_VALUES_LIST_SIZES[questionCodeIndex];
       currentQuestion = currentQuestion+1;
     } else {
       q2Code = QUESTION_CODE_LIST[questionCodeIndex];
       q2Values = QUESTION_VALUES_LIST[questionCodeIndex];
+      q2Size = QUESTION_VALUES_LIST_SIZES[questionCodeIndex];
     }
   } 
   else {
@@ -156,15 +165,13 @@ void updateLEDs() {
   digitalWrite(Q0_PIN, currentQuestion == 0? HIGH: LOW);
   digitalWrite(Q1_PIN, currentQuestion == 1? HIGH: LOW);
   digitalWrite(Q2_PIN, currentQuestion == 2? HIGH: LOW);
-
 }
 
 void loop() {
   int press = digitalRead(SW_pin);
   int xCoord = analogRead(X_pin);
   int yCoord = analogRead(Y_pin);
-  Serial.print("curr question \n");
-  Serial.print(currentQuestion);
+
   if (press == 0) {
     handlePress();
   }
@@ -176,15 +183,16 @@ void loop() {
     handleVertical(false);
   }
 
-  if (xCoord > 700) {
+  if (gameState == 1 && xCoord > 700) {
     handleHorizontal(true);
   }
 
-  if (xCoord < 400) {
+  if (gameState == 1 && xCoord < 400) {
     handleHorizontal(false);
   }
 
   updateLEDs();
+
   if (gameState == 0) {
       sevseg.setNumber(QUESTION_CODE_LIST[questionCodeIndex]);
   } else {
